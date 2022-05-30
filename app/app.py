cb94d6ff5de82f92f143c7360cb88f3daf8e433e
grrdlcs <99183580+grrdlcs@users.noreply.github.com>
2022-05-30 18:12:49 -0300

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    cursos=['PHP','Python','Java','Javascript']
+    data={
+        'titulo':'Index123',
+        'bienvenida':'Saludos!',
+        'cursos':cursos,
+        'numero_cursos':len(cursos)
+    }
+    return render_template('index.html',data=data)
+
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre,edad):
+    data={
+        'titulo':'Contacto',
+        'nombre': nombre,
+        'edad': edad
+    }
+    return render_template('contacto.html', data=data)
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "ok"
+
+def pagina_no_encontrada(error):
+    return render_template('404.html'), 404
+if __name__=='__main__':
+    app.add_url_rule('/query_string',view_func=query_string)
+    app.register_error_handler(404,pagina_no_encontrada)
+    app.run(debug=True, port=5000)
